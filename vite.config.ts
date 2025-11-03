a2aae079c893470ea0e44efcd32cd6bcb11137a7
anapc_pessoal <anabluoliveira@outlook.com>
2025-11-02 23:49:40 -0300

    arrumando vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('gantt-task-react')) {
-              return 'vendor-gantt';
-            }
-            if (id.includes('react-big-calendar')) {
-              return 'vendor-calendar';
-            }
-            if (id.includes('react-google-charts')) {
-              return 'vendor-google-charts';
-            }
-            if (id.includes('xlsx')) {
-              return 'vendor-xlsx';
-            }
-            return 'vendor'; 
-          }
-        },
-      },
-    },
-  },
 })
